f50eaf262d8c7a580de9647e34c8c7abe19b8b06
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2023-03-10 20:20:36 +0100

    add note and note list components

--- notes/client/src/app/app.module.ts
+++ notes/client/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FontStyleModalComponent } from './components/font-style-modal/font-style-modal.component';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
+import { NoteComponent } from './components/note/note.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FontStyleModalComponent
+    FontStyleModalComponent,
+    NotesListComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule,
