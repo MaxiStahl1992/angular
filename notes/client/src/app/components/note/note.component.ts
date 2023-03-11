297a9c9c452a0371292c1854a34b1a41bbdb865c
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2023-03-11 18:44:22 +0100

    add ellipssis overflow to text in notes list

--- notes/client/src/app/components/note/note.component.ts
+++ notes/client/src/app/components/note/note.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotesService } from '../../notes.service';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.scss']
 })
-export class NoteComponent {
+export class NoteComponent implements OnInit {
 
+  constructor(private notesService: NotesService) {}
+
+  ngOnInit() {
+
+  }
 }
